5be9b637a55db010bb130ac210d6954550a8565c
Abhay <abhay.work.02@gmail.com>
2024-12-24 00:00:23 +0530

    Added the fucntionality of pressing enter button

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let windVal = document.querySelector('#wind-value')
 let pressureVal = document.querySelector('#pressure-value')
 let weatherIcon = document.querySelector('#main-img')
 let feelsLikeBox = document.querySelector('.feels-like')
+let enterVal = document.querySelector('#enter-place')
 
 btn.addEventListener('click', async () => {
     let inputVal = document.querySelector('#enter-place').value
@@ -17,17 +18,40 @@ btn.addEventListener('click', async () => {
     let date = getDate()
     let { temp, humidity, iconId, dayVibe, windSpeed, pressure, feelsLike } = await getData()
     let { tempInCel, tempInFar } = convertTemp(temp)
-    let { flTempInCel, flTempInFar } = convertTemp(feelsLike)
+    let { tempInCel: newTemp } = convertTemp(feelsLike)
     let imgUrl = getImgUrl(iconId)
     date1.innerHTML = date
     tempShow.innerHTML = tempInCel.toFixed(1) + `<sup class="super-script1">°C</sup>`
     humidityVal.innerHTML = `${humidity}<sub>%</sub>`
     windVal.innerHTML = `${windSpeed}<sub>m/sec</sub>`
     pressureVal.innerHTML = `${pressure}<sub>hpa</sub>`
-    feelsLikeBox.innerHTML = `Feels like ${flTempInCel}`
+    feelsLikeBox.innerHTML = `Feels like ${newTemp.toFixed(1)}<sup class="super-script2">°C</sup>`
     // weatherIcon.setAttribute(src, imgUrl)
     // weatherIcon.src = imgUrl
     console.log(imgUrl)
+})
+
+enterVal.addEventListener('keyup', async (e) => {
+    if (e.key == 'Enter' && enterVal.value != '') {
+        let inputVal = document.querySelector('#enter-place').value
+        place = inputVal
+        place = place.toUpperCase()
+        placeName.innerHTML = place
+        let date = getDate()
+        let { temp, humidity, iconId, dayVibe, windSpeed, pressure, feelsLike } = await getData()
+        let { tempInCel, tempInFar } = convertTemp(temp)
+        let { tempInCel: newTemp } = convertTemp(feelsLike)
+        let imgUrl = getImgUrl(iconId)
+        date1.innerHTML = date
+        tempShow.innerHTML = tempInCel.toFixed(1) + `<sup class="super-script1">°C</sup>`
+        humidityVal.innerHTML = `${humidity}<sub>%</sub>`
+        windVal.innerHTML = `${windSpeed}<sub>m/sec</sub>`
+        pressureVal.innerHTML = `${pressure}<sub>hpa</sub>`
+        feelsLikeBox.innerHTML = `Feels like ${newTemp.toFixed(1)}<sup class="super-script2">°C</sup>`
+        // weatherIcon.setAttribute(src, imgUrl)
+        // weatherIcon.src = imgUrl
+        console.log(imgUrl)
+    }
 })
 
 async function getLatAndLon() {
