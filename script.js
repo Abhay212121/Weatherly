0106c209ecde9c6cd4e30a2ca2438421fc9f9235
Abhay <abhay.work.02@gmail.com>
2024-12-24 22:25:46 +0530

    Responsive 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var rootContext = document.body.getAttribute("data-root");
 let apiKey = 'b2235684c0a886fd774b800d3348856b';
 let btn = document.querySelector('#search')
 let placeName = document.querySelector('.place-name')
@@ -10,6 +11,8 @@ let weatherIcon = document.querySelector('#main-img')
 let feelsLikeBox = document.querySelector('.feels-like')
 let enterVal = document.querySelector('#enter-place')
 
+
+
 btn.addEventListener('click', async () => {
     let inputVal = document.querySelector('#enter-place').value
     place = inputVal
@@ -19,16 +22,15 @@ btn.addEventListener('click', async () => {
     let { temp, humidity, iconId, dayVibe, windSpeed, pressure, feelsLike } = await getData()
     let { tempInCel, tempInFar } = convertTemp(temp)
     let { tempInCel: newTemp } = convertTemp(feelsLike)
-    let imgUrl = getImgUrl(iconId)
+    let imgRef = getImgUrl(iconId)
     date1.innerHTML = date
     tempShow.innerHTML = tempInCel.toFixed(1) + `<sup class="super-script1">°C</sup>`
     humidityVal.innerHTML = `${humidity}<sub>%</sub>`
     windVal.innerHTML = `${windSpeed}<sub>m/sec</sub>`
     pressureVal.innerHTML = `${pressure}<sub>hpa</sub>`
     feelsLikeBox.innerHTML = `Feels like ${newTemp.toFixed(1)}<sup class="super-script2">°C</sup>`
-    // weatherIcon.setAttribute(src, imgUrl)
-    // weatherIcon.src = imgUrl
-    console.log(imgUrl)
+    weatherIcon.src = rootContext + 'images/' + imgRef
+    console.log(windSpeed)
 })
 
 enterVal.addEventListener('keyup', async (e) => {
@@ -37,20 +39,19 @@ enterVal.addEventListener('keyup', async (e) => {
         place = inputVal
         place = place.toUpperCase()
         placeName.innerHTML = place
-        let date = getDate()
         let { temp, humidity, iconId, dayVibe, windSpeed, pressure, feelsLike } = await getData()
+        let date = getDate()
         let { tempInCel, tempInFar } = convertTemp(temp)
         let { tempInCel: newTemp } = convertTemp(feelsLike)
-        let imgUrl = getImgUrl(iconId)
+        let imgRef = getImgUrl(iconId)
         date1.innerHTML = date
         tempShow.innerHTML = tempInCel.toFixed(1) + `<sup class="super-script1">°C</sup>`
         humidityVal.innerHTML = `${humidity}<sub>%</sub>`
         windVal.innerHTML = `${windSpeed}<sub>m/sec</sub>`
         pressureVal.innerHTML = `${pressure}<sub>hpa</sub>`
         feelsLikeBox.innerHTML = `Feels like ${newTemp.toFixed(1)}<sup class="super-script2">°C</sup>`
-        // weatherIcon.setAttribute(src, imgUrl)
-        // weatherIcon.src = imgUrl
-        console.log(dayVibe)
+        weatherIcon.src = 'Images/' + imgRef
+        console.log(iconId)
     }
 })
 
@@ -86,8 +87,29 @@ function convertTemp(temp) {
 }
 
 function getImgUrl(id) {
-    let url = "http://openweathermap.org/img/w/" + id + ".png";
-    return url
+    let ans;
+    if (id == '01d' || id == '01n') {
+        ans = 'sun.png'
+    }
+    else if (id == '02d' || id == '03d' || id == '04d' || id == '02n' || id == '03n' || id == '04n') {
+        ans = 'cloudy.png';
+    }
+    else if (id == '10d' || id == '10n' || id == '09d' || id == '09n') {
+        ans = 'rain.png'
+    }
+    else if (id == '11d' || id == '11n') {
+        ans = 'storm.png'
+    }
+    else if (id == '13d' || id == '13n') {
+        ans = 'snowflake.png'
+    }
+    else if (id == '50d' || id == '50n') {
+        ans = 'mist.png'
+    }
+    else if (id == '09d' || id == '09n') {
+        ans = 'drizzle.png'
+    }
+    return ans;
 }
 
 function getDate() {
